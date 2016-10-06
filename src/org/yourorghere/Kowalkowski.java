59c4c84ede759681288be159a8f0a9b013a23966
student <student@240-05.INFORMATYKA.local>
2016-10-06 13:31:13 +0200

    zad3a

--- src/org/yourorghere/Kowalkowski.java
+++ src/org/yourorghere/Kowalkowski.java
@@ -91,13 +91,14 @@ GL gl = drawable.getGL();
  gl.glLoadIdentity();
 
 // Tu piszemy kod rysuj¹cy grafikê 3D
- 
+ gl.glColor3f(1.0f,0.0f,0.0f);
  gl.glBegin(GL.GL_TRIANGLES);
 gl.glVertex3f(-1.0f, 1.0f, -6.0f);
 gl.glVertex3f(-2.0f,-1.0f, -6.0f);
 gl.glVertex3f( 0.0f,-1.0f, -6.0f);
 gl.glEnd();
 
+gl.glColor3f(0.0f,1.0f,0.0f);
 gl.glBegin(GL.GL_QUADS);
 gl.glVertex3f(1.0f, 1.0f, -16.0f);
 gl.glVertex3f(1.0f,-1.0f, -16.0f);
