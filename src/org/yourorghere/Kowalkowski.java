5e5099ba21accd425d44a430feb10ee3ab3f57f7
student <student@240-05.INFORMATYKA.local>
2016-10-06 13:22:44 +0200

    zad2

--- src/org/yourorghere/Kowalkowski.java
+++ src/org/yourorghere/Kowalkowski.java
@@ -98,6 +98,13 @@ gl.glVertex3f(-2.0f,-1.0f, -6.0f);
 gl.glVertex3f( 0.0f,-1.0f, -6.0f);
 gl.glEnd();
 
+gl.glBegin(GL.GL_QUADS);
+gl.glVertex3f(1.0f, 1.0f, -6.0f);
+gl.glVertex3f(1.0f,-1.0f, -6.0f);
+gl.glVertex3f( 2.0f,-1.0f, -6.0f);
+gl.glVertex3f( 2.0f,1.0f, -6.0f);
+gl.glEnd();
+
 
  //Wykonanie wszystkich operacji znajduj¹cych siê w buforze
  gl.glFlush();
